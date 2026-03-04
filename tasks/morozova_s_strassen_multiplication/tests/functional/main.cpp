84703b3324b094118103c28af0dd7098f43c3e41
SonyaMorozova <sonyasmorozova@gmail.com>
2026-03-04 16:03:00 +0300

    fix: clang-tidy

--- tasks/morozova_s_strassen_multiplication/tests/functional/main.cpp
+++ tasks/morozova_s_strassen_multiplication/tests/functional/main.cpp
@@ -10,6 +10,7 @@
 #include "morozova_s_strassen_multiplication/common/include/common.hpp"
 #include "morozova_s_strassen_multiplication/seq/include/ops_seq.hpp"
 #include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace morozova_s_strassen_multiplication {
 
